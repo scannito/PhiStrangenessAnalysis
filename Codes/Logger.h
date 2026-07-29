da0305233fa267555c2e6cabab681c801ad7a3a8
scannito <stefano.cannito@cern.ch>
2026-07-29 18:07:58 +0200

    spdlog-based Logger (1)

---
+++ Codes/Logger.h
@@ -0,0 +1,14 @@
+#pragma once
+
+// Tell spdlog to use the C++20 standard
+#define SPDLOG_USE_STD_FORMAT
+
+#include "spdlog/spdlog.h"
+
+#include <string>
+#include <string_view>
+
+class Logger
+{
+  // The code
+};
